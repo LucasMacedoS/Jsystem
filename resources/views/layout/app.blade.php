2b378865952d91aca99147627a938fc45e001539
Lucas Macedo <lucasmacedonsantos1998@gmail.com>
2019-02-18 15:29:05 -0300

    layout + home

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+
+  <!-- Título -->
+  <title> @yield('title') </title>
+
+
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Styles -->
+  <!-- Font Awesome -->
+  <!-- <link rel="stylesheet" href="{{ asset('theme/plugins/font-awesome/css/font-awesome.min.css') }}"> -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{ asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}">
+
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('theme/css/adminlte.min.css') }}">
+
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/iCheck/flat/blue.css') }}">
+
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/morris/morris.css') }}">
+
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/datepicker/datepicker3.css') }}">
+
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/daterangepicker/daterangepicker-bs3.css') }}">
+
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="{{ asset('theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+
+  <!-- Google Font: Source Sans Pro -->
+  <link href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700') }}" rel="stylesheet">
+
+
+
+  <!-- Scripts -->
+  <!-- jQuery -->
+  <script src="{{ asset('theme/plugins/jquery/jquery.min.js') }}"></script>
+
+  <!-- jQuery UI 1.11.4 -->
+  <script src="{{ asset('https://code.jquery.com/ui/1.12.1/jquery-ui.min.js') }}"></script>
+
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <!-- Bootstrap 4 -->
+  <script src="{{ asset('theme/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+
+  <!-- Morris.js charts -->
+  <script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js') }}"></script>
+
+  <script src="{{ asset('theme/plugins/morris/morris.min.js') }}"></script>
+
+  <!-- Sparkline -->
+  <script src="{{ asset('theme/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+
+  <!-- jvectormap -->
+  <script src="{{ asset('theme/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+  <script src="{{ asset('theme/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+
+  <!-- jQuery Knob Chart -->
+  <script src="{{ asset('plugins/knob/jquery.knob.js') }}"></script>
+
+  <!-- daterangepicker -->
+  <script src="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js') }}"></script>
+  <script src="{{ asset('theme/plugins/daterangepicker/daterangepicker.js') }}"></script>
+
+  <!-- datepicker -->
+  <script src="{{ asset('theme/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+
+  <!-- Bootstrap WYSIHTML5 -->
+  <script src="{{ asset('theme/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+
+  <!-- Slimscroll -->
+  <script src="{{ asset('theme/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+
+  <!-- FastClick -->
+  <script src="{{ asset('theme/plugins/fastclick/fastclick.js') }}"></script>
+
+  <!-- AdminLTE App -->
+  <script src="{{ asset('theme/js/adminlte.js')}}"></script>
+
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="{{ asset('theme/js/pages/dashboard.js') }}"></script>
+
+  <!-- AdminLTE for demo purposes -->
+  <script src="{{ asset('theme/js/demo.js') }}"></script>
+
+</head>
+
+
+
+
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand bg-danger navbar-light border-bottom">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+      </li>
+
+
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <!-- <form class="form-inline ml-3">
+      <div class="input-group input-group-sm">
+        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-navbar" type="submit">
+            <i class="fa fa-search"></i>
+          </button>
+        </div>
+      </div>
+    </form> -->
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+      <li class="nav-item active">
+        <a class="nav-link" href=""> <i class="fas fa-sign-in-alt"></i> Login </a>
+      </li>
+
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+
+
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link bg-danger">
+      <img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-light"> JSystem </span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+          <li class="nav-header"> Menu </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-receipt"></i>
+              <p> Comanda </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="fas fa-tag"></i>
+              <p> Produtos </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <!-- Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark"> @yield('header', 'Home') </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Main content -->
+    <section class="content">
+
+      <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>15</h3>
+
+                <p> Comandas ativas </p>
+              </div>
+              <div class="icon">
+                <i class="fas fa-receipt"></i>
+              </div>
+              <a href="#" class="small-box-footer"> Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                <p> Balanço Semanal </p>
+              </div>
+              <div class="icon">
+                <i class="far fa-chart-bar"></i>
+              </div>
+              <a href="#" class="small-box-footer"> Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+              <div class="inner">
+                <h3>44</h3>
+
+                <p> Funcionários </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-add"></i>
+              </div>
+              <a href="#" class="small-box-footer"> Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>65</h3>
+
+                <p> Relatórios </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-pie-graph"></i>
+              </div>
+              <a href="#" class="small-box-footer"> Mais informações <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+
+        <br>
+
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-7 connectedSortable">
+
+
+
+            <!-- TO DO List -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">
+                  <i class="ion ion-clipboard mr-1"></i>
+                  To Do List
+                </h3>
+
+                <div class="card-tools">
+                  <ul class="pagination pagination-sm">
+                    <li class="page-item"><a href="#" class="page-link">&laquo;</a></li>
+                    <li class="page-item"><a href="#" class="page-link">1</a></li>
+                    <li class="page-item"><a href="#" class="page-link">2</a></li>
+                    <li class="page-item"><a href="#" class="page-link">3</a></li>
+                    <li class="page-item"><a href="#" class="page-link">&raquo;</a></li>
+                  </ul>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <ul class="todo-list">
+                  <li>
+                    <!-- drag handle -->
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <!-- checkbox -->
+                    <input type="checkbox" value="" name="">
+                    <!-- todo text -->
+                    <span class="text">Design a nice theme</span>
+                    <!-- Emphasis label -->
+                    <small class="badge badge-danger"><i class="fa fa-clock-o"></i> 2 mins</small>
+
+
+
+                    <!-- Ferramentas de editar e deletar -->
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <input type="checkbox" value="" name="">
+                    <span class="text">Make the theme responsive</span>
+                    <small class="badge badge-info"><i class="fa fa-clock-o"></i> 4 hours</small>
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <input type="checkbox" value="" name="">
+                    <span class="text">Let theme shine like a star</span>
+                    <small class="badge badge-warning"><i class="fa fa-clock-o"></i> 1 day</small>
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <input type="checkbox" value="" name="">
+                    <span class="text">Let theme shine like a star</span>
+                    <small class="badge badge-success"><i class="fa fa-clock-o"></i> 3 days</small>
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <input type="checkbox" value="" name="">
+                    <span class="text">Check your messages and notifications</span>
+                    <small class="badge badge-primary"><i class="fa fa-clock-o"></i> 1 week</small>
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                  <li>
+                    <span class="handle">
+                      <i class="fa fa-ellipsis-v"></i>
+                      <i class="fa fa-ellipsis-v"></i>
+                    </span>
+                    <input type="checkbox" value="" name="">
+                    <span class="text">Let theme shine like a star</span>
+                    <small class="badge badge-secondary"><i class="fa fa-clock-o"></i> 1 month</small>
+                    <div class="tools">
+                      <i class="fa fa-edit"></i>
+                      <i class="fa fa-trash-o"></i>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+
+              <!-- End Body Card -->
+              <div class="card-footer clearfix">
+                <button type="button" class="btn btn-info float-right"><i class="fa fa-plus"></i> Adicionar item</button>
+              </div>
+
+            </div>
+            <!-- End Card -->
+
+          </section>
+        </div>
+      </div>
+    </section>
+  </div>
+
+
+  <footer class="main-footer">
+
+    <div class="text-center">
+      <strong>Copyright &copy; 2018-2019 Jegue's Bar System  .</strong>
+      Todos os direitos reservados.
+    </div>
+
+    <div class="float-right d-none d-sm-inline-block">
+      <b> Version </b> 2.0.0
+    </div>
+
+  </footer>
+
+
+
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+
+</body>
+</html>
